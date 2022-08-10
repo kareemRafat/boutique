dd760e87a787f01bf68e916ac088254fe6875c2c
kareemRafat <kareem.force@gmail.com>
2022-08-10 20:51:39 +0200

    fix controller nam

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ShopController;
+use App\Http\Controllers\Design\ShopController;
 
 /*
 |--------------------------------------------------------------------------
