2a1eaeeaf95219a1e8ba083da978b920aa83f640
= <kareem.force@gmail.com>
2022-08-28 00:10:51 +0200

    finish product update

--- app/Http/Requests/Admin/ProductRequest.php
+++ app/Http/Requests/Admin/ProductRequest.php
@@ -23,12 +23,32 @@ class ProductRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'name' => 'required|min:4|max:20',
-            'price' => 'required|numeric',
-            'stock' => 'required|numeric',
-            'description' => 'required',
-            'cat_id' => 'required|numeric',
-        ];
+
+        // use this to know if i add a new product or update a product
+
+        $pro_id = $this->segment(2); //[get the product id]
+
+        switch ($pro_id) {
+            case 'null':
+                return [
+                    'name' => 'required|min:4|max:20',
+                    'price' => 'required|numeric',
+                    'stock' => 'required|numeric',
+                    'description' => 'required',
+                    'cat_id' => 'required|numeric',
+                ];
+                break;
+
+            case $pro_id:
+
+                return [
+                    'name' => 'required|min:4|max:10',
+                    'price' => 'required|numeric',
+                    'stock' => 'required|numeric',
+                    'description' => 'required',
+                    'cat_id' => 'required|numeric',
+                ];
+                break;
+        }
     }
 }
